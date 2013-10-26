816a8718e82459452fc8f7b4ec082ee1ee09c730
johntalent <talent.corp@gmail.com>
2013-10-26 09:33:31 -0500

    Revisión general

--- app/models/usertype.rb
+++
@@ -1,2 +0,0 @@
-class Usertype < ActiveRecord::Base
-end
--- db/migrate/20130530230822_create_usertypes.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsertypes < ActiveRecord::Migration
-  def change
-    create_table :usertypes do |t|
-
-      t.timestamps
-    end
-  end
-end
