51d0739b977744fd96429524f9c6b882cdc8b66f
johntalent <talent.corp@gmail.com>
2013-11-24 11:14:58 -0500

    Adición del directory de Templates admin

--- evolucion/projects/models.py
+++ evolucion/projects/models.py
@@ -4,12 +4,13 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
     pub_date = models.DateTimeField('date published')
     def __unicode__(self):
         return self.title
 
 class Prose(models.Model):
-    project = models.ForeignKey(Project)
+    project = models.OneToOneField(Project, primary_key=True)
     title = models.CharField(max_length=200)
     description = models.TextField()
     def __unicode__(self):
