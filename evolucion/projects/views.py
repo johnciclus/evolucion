035d7cd3d74304cea3455bd151e9560828adbaf1
johntalent <talent.corp@gmail.com>
2013-12-02 11:28:48 -0500

    Adición Bootstrap

--- evolucion/projects/views.py
+++ evolucion/projects/views.py
@@ -2,13 +2,14 @@ import sys
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from datetime import datetime
+from django.views import generic
+from django.utils import timezone
+
 from evolucion.projects.models import Project
 
 class IndexView(generic.ListView):
     template_name = 'projects/index.html'
     context_object_name = 'latest_projects_list'
-
     def get_queryset(self):
         return Project.objects.order_by('-pub_date')[:5]
 
@@ -24,11 +25,12 @@ def save(request):
         
         print request.POST
         
-        p = Project(title=title, description=description, pub_date=datetime.today())
+        p = Project(title=title, description=description, pub_date=timezone.now())
         p.save()
         latest_projects_list = Project.objects.order_by('-pub_date')[:5]
         context = {'latest_projects_list': latest_projects_list}
-        return render(request, 'projects/index.html', context)
+        #return render(request, 'projects/index.html', context)
+        return HttpResponseRedirect('/projects/')
 
 #def detail(request, pk):
 #    project = get_object_or_404(Project, pk=pk)
